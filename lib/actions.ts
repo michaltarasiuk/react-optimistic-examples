ec61c84a1939fdb7459f0ce91af869c57936c555
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-01 20:44:08 +0200

    decrease network delay

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
+const NETWORK_DELAY_MS = 500;
+
 export async function updateAmount(newAmount: number) {
-  await new Promise((resolve) => setTimeout(resolve, 2_000));
+  await new Promise((resolve) => setTimeout(resolve, NETWORK_DELAY_MS));
   return newAmount;
 }
